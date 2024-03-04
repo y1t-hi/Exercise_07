f54eff108d7aeeff33844e449c259d43c7128870
Wyatt Compton <58642034+y1t-hi@users.noreply.github.com>
2024-03-03 22:53:28 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+//Name : Wyatt Compton
+//Exercise : Exercise07
+//Date : 3/3/2024
+    public static void main(String[] args) {
+        Contact contact1 = new Contact
+                ("billy bob", "billy bob joe@gmail.com");
+        BuisnessContact businessDude = new BuisnessContact
+                ("Nick", "Nickthepoliceman@yahoo.com", "911");
+
+        contact1.display();
+        businessDude.display();
+
+
+
+
+    }
+}
